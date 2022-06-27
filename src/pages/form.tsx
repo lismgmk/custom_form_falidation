b14408c024a1fc78ed819aa6c63152aa455a77cc
lismgmk <101820684+uladzimirLis@users.noreply.github.com>
2022-06-27 13:53:25 +0300

    add styles

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,9 +1,22 @@
 import useForm from './hooks/useForm';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './form.scss';
 
 export function Form() {
-  const formLogin = () => {
+  const [load, setLoad] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setShow(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [show]);
+
+  const formLogin = async () => {
     if (
       errors.phone === '' &&
       errors.email === '' &&
@@ -16,56 +29,113 @@ export function Form() {
       values.username !== '' &&
       values.textArea !== ''
     ) {
-      alert('!!!!!!');
-      setValues({ email: '', username: '', phone: '', date: '', textArea: '' });
+      try {
+        setLoad(true);
+        const response = await fetch('https://simple-express-inc.herokuapp.com/form', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(values),
+        });
+        setShow(true);
+        if (!response.ok) {
+          const errorMessage = await response.text().then((e) => JSON.parse(e));
+          setResponseMessage(`Код ответа ${response.status} ${errorMessage.message}`);
+        }
+        const responseText = await response.text();
+        const messageRes = JSON.parse(responseText);
+        setResponseMessage(`Код ответа ${response.status} ${messageRes.message}`);
+        setValues({ email: '', username: '', phone: '', date: '', textArea: '' });
+      } catch (ex) {
+        console.error('POST error!');
+      } finally {
+        setLoad(false);
+      }
     }
   };
 
   const { handleChange, values, errors, handleSubmit, setValues } = useForm(formLogin);
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit} noValidate>
-        <input
-          autoComplete="off"
-          type="text"
-          value={values.username}
-          name="username"
-          placeholder="USERNAME SURNAME"
-          onChange={handleChange}
-        />
-        {errors.username && <h3>{errors.username}</h3>}
-        <input
-          type="email"
-          autoComplete={'off'}
-          name="email"
-          placeholder="example@mail.com"
-          value={values.email}
-          onChange={handleChange}
-        />
-        {errors.email && <h3>{errors.email}</h3>}
-        <input
-          autoComplete={'off'}
-          value={values.phone}
-          type="tel"
-          name="phone"
-          placeholder="(123)-(456)-(7891)"
-          onChange={handleChange}
-        />
-        {errors.phone && <h3>{errors.phone}</h3>}
-        <input
-          type="date"
-          min="1920-01-01"
-          max="2016-12-31"
-          name="date-birth"
-          value={values.date}
-          onChange={handleChange}
-        />
-        {errors.date && <h3>{errors.date}</h3>}
-        <textarea rows={10} cols={50} name="textarea" value={values.textArea} onChange={handleChange} />
-        {errors.textArea && <h3>{errors.textArea}</h3>}
-        <input type="submit" value="Submit" className="submit" />
-      </form>
+    <div className={'main-container'}>
+      {show && (
+        <div className="modal">
+          <div className="modal-content">
+            <span onClick={() => setShow(false)} className="close-button">
+              &times;
+            </span>
+            <h2>{responseMessage}</h2>
+            <p>Нажмите закрыть, либо окно закроется через 3 секунды автоматически</p>
+          </div>
+        </div>
+      )}
+      <div className="container">
+        <form className="form" onSubmit={handleSubmit} noValidate>
+          <div className="form-control">
+            <label htmlFor="username">Имя фамилия</label>
+            <input
+              autoComplete="off"
+              type="text"
+              value={values.username}
+              name="username"
+              placeholder="ИМЯ ФАМИЛИЯ"
+              onChange={handleChange}
+            />
+            {errors.username && <small>{errors.username}</small>}
+          </div>
+          <div className="form-control">
+            <label htmlFor="username">Электронная почта</label>
+            <input
+              type="email"
+              autoComplete={'off'}
+              name="email"
+              placeholder="example@mail.com"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && <small>{errors.email}</small>}
+          </div>
+          <div className="form-control">
+            <label htmlFor="username">Телефон</label>
+            <input
+              autoComplete={'off'}
+              value={values.phone}
+              type="tel"
+              name="phone"
+              placeholder="(123)-(456)-(7891)"
+              onChange={handleChange}
+            />
+            {errors.phone && <small>{errors.phone}</small>}
+          </div>
+          <div className="form-control">
+            <label htmlFor="username">Дата рождения</label>
+            <input
+              type="date"
+              min="1920-01-01"
+              max="2016-12-31"
+              name="date-birth"
+              value={values.date}
+              onChange={handleChange}
+            />
+            {errors.date && <small>{errors.date}</small>}
+          </div>
+          <div className="form-control">
+            <label htmlFor="username">Сообщение</label>
+            <textarea rows={7} cols={40} name="textarea" value={values.textArea} onChange={handleChange} />
+            {errors.textArea && <small>{errors.textArea}</small>}
+          </div>
+
+          {load ? (
+            <button>Идет отправка...</button>
+          ) : (
+            <button type="submit" value="Submit" className="submit">
+              Отправить
+            </button>
+          )}
+          <span>* что бы протестировать ошибку от сервера отправить номер телефона (000)-000-000</span>
+        </form>
+      </div>
     </div>
   );
 }
